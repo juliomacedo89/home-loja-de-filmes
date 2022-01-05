2370570bd2656a4894a2eb704497ae7c31e2de8d
Júlio Macedo <juliocesarmacedo@icloud.com>
2022-01-05 11:55:46 -0300

    first commit

---
+++ src/components/modal/index.js
@@ -0,0 +1,42 @@
+import './style';
+
+$(document).ready(function() {
+      
+      $('.banner__container-carousel').slick({
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            centerMode: false,
+            autoplay: true,
+            autoplaySpeed: 2000,
+            prevArrow: $("#arrow-prev"),
+            nextArrow: $("#arrow-next"),
+            responsive: [
+            {
+                  breakpoint: 640,
+                  settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 3,
+                  infinite: true,
+                  prevArrow: $("#arrow-prev"),
+                  nextArrow: $("#arrow-next")
+                  
+                  }
+            }]
+            });          
+
+
+      $('.brands__container-carousel').slick({
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            autoplay: true,
+            infinite: true,
+            autoplaySpeed: 1000,
+            arrow: false,
+            prevArrow: false,
+            nextArrow: false,
+            
+      })
+
+
+})
