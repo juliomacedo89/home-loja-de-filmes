88c95a4d0627fd26d9f32dad931581c571ab6818
Júlio Macedo <juliocesarmacedo@icloud.com>
2022-01-06 21:48:58 -0300

    second commit

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -23,7 +23,7 @@ $(document).ready(function() {
                   
                   }
             }]
-            });          
+            });   
 
 
       $('.brands__container-carousel').slick({
@@ -34,9 +34,39 @@ $(document).ready(function() {
             autoplaySpeed: 1000,
             arrow: false,
             prevArrow: false,
-            nextArrow: false,
+            nextArrow: false
             
       })
 
-
+      function moving() {
+            var body = document.querySelector('body');
+            var currentTop = 0;
+            var oldTop = 0;
+            var headerTop = document.querySelector('#header').offsetHeight;
+        
+            body.setAttribute('data-scrolling', false);
+            body.setAttribute('data-scrolling-mode', 'none');
+        
+            window.addEventListener('scroll', () => { 
+                currentTop = window.pageYOffset;
+                
+                if(currentTop == 0) 
+                  body.setAttribute('data-scrolling', false);    
+                else   
+                body.setAttribute('data-scrolling', true);                 
+        
+                if(currentTop > headerTop) {
+                    if(oldTop > currentTop) {
+                        body.setAttribute('data-scrolling-mode', 'up');
+                    } else if (oldTop < currentTop) {   
+                        body.setAttribute('data-scrolling-mode', 'down');
+                    } else {
+                        body.setAttribute('data-scrolling-mode', 'none'); 
+                    }
+                    oldTop = currentTop;
+                } else body.setAttribute('data-scrolling-mode', 'none');
+            });
+        }
+        
+        moving()
 })
