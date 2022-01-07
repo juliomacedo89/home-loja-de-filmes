9b1df10649bcb8d9b8df88713d87df44159701fe
Júlio Macedo <juliocesarmacedo@icloud.com>
2022-01-07 08:52:12 -0300

    last commit

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
                   breakpoint: 640,
                   settings: {
                   slidesToShow: 1,
-                  slidesToScroll: 3,
+                  slidesToScroll: 2,
                   infinite: true,
                   prevArrow: $("#arrow-prev"),
                   nextArrow: $("#arrow-next")
@@ -34,39 +34,32 @@ $(document).ready(function() {
             autoplaySpeed: 1000,
             arrow: false,
             prevArrow: false,
-            nextArrow: false
+            nextArrow: false,
+            responsive: [
+            {
+                  breakpoint: 640,
+                  settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 2,
+                  infinite: true,
+                  prevArrow: $("#arrow-prev"),
+                  nextArrow: $("#arrow-next")
+                  
+                  }
+            },
+            {
+                  breakpoint: 600,
+                  settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 1,
+                  infinite: true,
+                  prevArrow: $("#arrow-prev"),
+                  nextArrow: $("#arrow-next")
+                  
+                  }
+            }]
             
       })
 
-      function moving() {
-            var body = document.querySelector('body');
-            var currentTop = 0;
-            var oldTop = 0;
-            var headerTop = document.querySelector('#header').offsetHeight;
-        
-            body.setAttribute('data-scrolling', false);
-            body.setAttribute('data-scrolling-mode', 'none');
-        
-            window.addEventListener('scroll', () => { 
-                currentTop = window.pageYOffset;
-                
-                if(currentTop == 0) 
-                  body.setAttribute('data-scrolling', false);    
-                else   
-                body.setAttribute('data-scrolling', true);                 
-        
-                if(currentTop > headerTop) {
-                    if(oldTop > currentTop) {
-                        body.setAttribute('data-scrolling-mode', 'up');
-                    } else if (oldTop < currentTop) {   
-                        body.setAttribute('data-scrolling-mode', 'down');
-                    } else {
-                        body.setAttribute('data-scrolling-mode', 'none'); 
-                    }
-                    oldTop = currentTop;
-                } else body.setAttribute('data-scrolling-mode', 'none');
-            });
-        }
-        
-        moving()
+      
 })
